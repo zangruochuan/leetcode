3523c6c6084945c4c17e53ab90bd33cc579ac731
zrc <zrc@123.com>
2019-01-31 15:30:14 +0800

    26.remove-duplicates-from-sorted-array

--- 26.remove-duplicates-from-sorted-array.cs
+++ 26.remove-duplicates-from-sorted-array.cs
@@ -53,6 +53,20 @@
  */
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        
+        if (nums.Length==0)
+        {
+            return 0;
+        }
+        int j = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            
+            if (nums[i]!=nums[j])
+            {                
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j+1;
     }
 }
